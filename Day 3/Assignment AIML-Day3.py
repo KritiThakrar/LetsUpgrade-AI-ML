4ec960c1944a6b77f819adf6ec7076cb92b2d413
kriti-thakrar <66572570+kriti-thakrar@users.noreply.github.com>
2020-07-10 03:35:46 +0530

    Add files via upload

---
+++ Day 3/Assignment AIML-Day3.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Question 1
+A=complex(2,4)
+B=complex(6,12)
+C=A-B
+print("Sutraction =",C)
+
+
+# In[6]:
+
+
+#Question 2
+import math
+a=12
+rt=math.pow(a,0.25)
+print(rt)
+
+
+# In[11]:
+
+
+#Question 3
+x=5
+y=6
+temp=x
+x=y
+y=temp
+print("The value of x is: ",temp)
+print("The value of y is: ",x)
+print("The value of x after swapping is: ",x)
+print("The value of y after swapping is: ",y)
+
+
+# In[12]:
+
+
+#Question 4
+x=5
+y=6
+x ,y=y ,x
+print("The value of x after swapping is: ",x)
+print("The value of y after swapping is: ",y)
+
+
+# In[14]:
+
+
+#Question 5
+f=98
+c=(f-32)*5/9
+k=c+273.15
+print("Celcius",c)
+print("Kelvin",k)
+
+
+# In[ ]:
+
+
+#Question 6
+
+
+# In[16]:
+
+
+type(12)
+
+
+# In[17]:
+
+
+type(12.2)
+
+
+# In[18]:
+
+
+type("Kriti")
+
+
+# In[19]:
+
+
+type([1,2,3,4])
+
+
+# In[20]:
+
+
+type({1:"Kriti"})
+
+
+# #Question 7
+# Steps to create a github profile and upload the link
+# Step 1: Sign up to github if you dont have an account.
+# Step 2: Create a folder named by number of day you are doing the assignment and put the assignment in that folder.
+# Step 3: Go to new option near Repository and create a new repository named LetsUpgrade-AI-ML
+# Step 4: Go to "upload an existing file" option
+# step 5: Drag and drop the folder and click on comit changes
+# Step 6: Copy the link of the file and paste it in the assignment submission file
+
+# In[ ]:
+
+
+
+
